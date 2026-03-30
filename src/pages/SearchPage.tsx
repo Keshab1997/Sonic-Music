28271abaa571d9cdd6236b153e1c2c415686c65f
Keshab1997 <keshabsarkar2018@gmail.com>
2026-03-31 00:28:43 +0530

    Use youtube-nocookie.com for embed to avoid playback restrictions

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -149,7 +149,7 @@ export const SearchPage = () => {
                     <div className="mt-2 mb-4 rounded-xl overflow-hidden bg-black border border-border">
                       <div className="relative w-full" style={{ paddingBottom: "56.25%" }}>
                         <iframe
-                          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0`}
+                          src={`https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&rel=0`}
                           title={track.title}
                           className="absolute top-0 left-0 w-full h-full"
                           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
