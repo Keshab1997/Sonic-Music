c3829b58ef7129cdd913105f10b66c2c8517dfd4
Keshab1997 <keshabsarkar2018@gmail.com>
2026-03-31 10:14:54 +0530

    Fix sidebar visibility on desktop - add flex-shrink-0 and md:flex-col

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -38,7 +38,7 @@ export const AppSidebar = () => {
   };
 
   return (
-    <aside className="hidden md:flex flex-col w-64 lg:w-72 bg-sidebar border-r border-sidebar-border h-full overflow-hidden">
+    <aside className="hidden md:flex md:flex-col w-64 lg:w-72 bg-sidebar border-r border-sidebar-border h-full overflow-hidden flex-shrink-0">
       {/* Logo */}
       <div className="p-6 pb-4 flex items-center justify-between">
         <Link to="/">
